d6fead9b80cf1b490d50e68033db0d3f3ba660a0
Manh Kien <manhkien3202@gmail.com>
2022-06-19 11:37:10 +0700

    first commit

---
+++ .history/tailwind.config_20220619104601.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        'shadowCustom': 'box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
+      }
+    },
+  },
+  plugins: [],
+}
