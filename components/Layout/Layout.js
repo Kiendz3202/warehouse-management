9ef1750575a46b73d43d050aca1e2e555f38c3fb
Manh Kien <manhkien3202@gmail.com>
2022-08-15 21:20:13 +0700

    fix w full

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -3,7 +3,7 @@ import Header from '../Header/Header'
 
 function Layout({ children }) {
     return (
-        <div>
+        <div className=' w-full bg-blue-500'>
             <div className=" max-w-[1536px] mx-auto pb-[70px] bg-blue-500 text-white">
                 <Header />
                 <main className=''>
